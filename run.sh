118e2b420fa33cc9ac20492f27f5d553449393b9
Hernawan Fa'iz Abdillah <hernawan.faiz.abdillah@gmail.com>
2021-03-31 11:08:00 +0700

    Add docker run script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+docker run --rm \
+     -ti -e DISPLAY=$DISPLAY \
+     -v $(realpath ./omnetws):/home/userpp/workspace \
+     -e OMNETPY_ROOT=/home/userpp/workspace/omnetpy/omnetpy \
+     --privileged \
+     -v /tmp/.X11-unix:/tmp/.X11-unix \
+     mmodenesi/omnetpy bash

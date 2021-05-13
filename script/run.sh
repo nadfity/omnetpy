dff19f5ed886c36a68746ca163d2a72d941aea03
Hernawan Fa'iz Abdillah <hernawan.faiz.abdillah@gmail.com>
2021-05-13 18:33:35 +0700

    Change to enable long-lived service

--- script/run.sh
+++ script/run.sh
@@ -11,11 +11,5 @@ rootdir=$(git -C "$(dirname $0)" rev-parse --show-toplevel)
 workspace=$(realpath $1)
 workspace=${workspace:-$(pwd)}
 
-docker run --rm \
-     -ti -e DISPLAY=$DISPLAY \
-     -v $workspace:/home/userpp/workspace \
-     -v $(realpath $rootdir/omnetpy):/home/userpp/omnetpy \
-     -e OMNETPY_ROOT=/home/userpp/workspace/omnetpy/omnetpy \
-     --privileged \
-     -v /tmp/.X11-unix:/tmp/.X11-unix \
-     nadfity/omnetpy bash
+DISPLAY=$DISPLAY workspace=$workspace omnetpydir=$(realpath $rootdir/omnetpy) docker-compose -f $rootdir/docker-compose.yml up -d
+docker-compose -f $rootdir/docker-compose.yml exec omnetpyenv bash
--- script/run_mac.sh
+++ script/run_mac.sh
@@ -13,12 +13,5 @@ rootdir=$(git -C "$(dirname $0)" rev-parse --show-toplevel)
 workspace=$(realpath $1)
 workspace=${workspace:-$(pwd)}
 
-docker run --rm \
-     -ti \
-     -e DISPLAY=host.docker.internal:0 \
-     -v $workspace:/home/userpp/workspace \
-     -v $(realpath $rootdir/omnetpy):/home/userpp/omnetpy \
-     -e OMNETPY_ROOT=/home/userpp/workspace/omnetpy/omnetpy \
-     --privileged \
-     -v /tmp/.X11-unix:/tmp/.X11-unix \
-     nadfity/omnetpy bash
+DISPLAY=$DISPLAY workspace=$workspace omnetpydir=$(realpath $rootdir/omnetpy) docker-compose -f $rootdir/docker-compose.yml up -d
+docker-compose -f $rootdir/docker-compose.yml exec omnetpyenv bash
